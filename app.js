ea5ce6e820052cc50256f0ddeed70760b92f6279
Mahmoud Elkabbany <mahmoudelkabbany292@gmail.com>
2022-07-14 12:02:49 +0200

    Add initial sqaures and adjusted styling

---
+++ app.js
@@ -0,0 +1,12 @@
+const CONTAINER_WIDTH = 560;
+
+const sketchBoard = document.querySelector(".container");
+
+for (let i = 1; i <= 16; i++) {
+  for (let i = 1; i <= 16; i++) {
+    const sketchSquare = document.createElement("div");
+    sketchSquare.style.width = `${CONTAINER_WIDTH / 16}px`;
+    sketchSquare.style.height = `${CONTAINER_WIDTH / 16}px`;
+    sketchBoard.appendChild(sketchSquare);
+  }
+}
